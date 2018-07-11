4807cd2262b15edee6e0a7a3180f4878c1d6fa37
AngelaIp <32091085+AngelaIp@users.noreply.github.com>
2018-07-11 09:31:11 +0200

    Update AIP_get_SSRS_Report.cs
    - Added option to get credentials from InnovatorServerConfig.xml
    - Improved AML queries to reduce server calls
    - Added error handlng for WebRequests and File handling
    - Added sequence for Document numbering

--- AIP_get_SSRS_Report.cs
+++ AIP_get_SSRS_Report.cs
@@ -1,96 +1,155 @@
-// AIP / b+ @ 2017-10-16
-// Method: AIP_Get_SSRS_Report
-
 // This sample code shows how to:
 // - Call the SQL report server from Aras
 // - Rename SQL Report
 // - Load the report result to the Aras Vault
 // - Create a new Document with the attached report
 
-// This sample works direct from a Method.
+// This sample works direct from a Method
 // With some simple modifications it can easy be used in an Action or Form button onClick event.
 
 Innovator inn = this.getInnovator();
 
-string itemID = "1234567891234567897B26F33247F90C"; // Id of Part
+string itemID = "1234567891234567897B26F33247F90C"; // Id of Part for testing
 
-// Define Report server
+// Get credentials from InnovatorServerConfig.xml
+XmlDocument xml = CCO.Cache.GetCacheInfo("ApplicationXML");
+string targetURL = "";
+string reportServerURL = xml.SelectSingleNode("//ReportingServices/ReportServer").InnerText;
+string reportUserDomain = xml.SelectSingleNode("//ReportingServices/Domain").InnerText;
+string reportUser = xml.SelectSingleNode("//ReportingServices/User").InnerText;
+//string reportUserPW = xml.SelectSingleNode("//ReportingServices/Password").InnerText; // works only with unencrypted PWs
+// Pitfall: A SecureString object should never be constructed from a String, because the sensitive data is already subject to the memory persistence consequences of the immutable String class. 
+
+SecureString securedPW = new SecureString(); 
+foreach (char c in xml.SelectSingleNode("//ReportingServices/Password").InnerText) // <-  is this really a good approach??
+{
+    securedPW.AppendChar(c);
+}
+securedPW.MakeReadOnly();
+
+/* debug - define credentials manually
+string reportServerURL = "http://myreportserver/SSRS/";
+string reportUserDomain = "";
 string reportUser = "myuser";
 string reportUserPW = "mypassword";
-string reportUserDomain = "";
-string targetURL = "http://myreportserver/SSRS/";
+*/
+
+// Define Report server
 string subfolder = "MYSUBFOLDER";
-string report = "MYBOMREPORT"; // Name of .rdl
+string report = "PCBA_BOM"; // Name of .rdl
 string command = "Render";
 string format = "EXCEL";
-string fileExtension = "xlsx";
+string fileExtension = "xls";
 
 // Get properties from request item
-Item myItem = inn.newItem("Part","get"); // select Email template
+Item myItem = inn.newItem("Part","get");
+myItem.setAttribute("select", "item_number,major_rev");
 myItem.setProperty("id",itemID);
-myItem=myItem.apply();
+myItem = myItem.apply();
+if (myItem.isError())
+{
+    return inn.newError("An error occured: " + myItem.getErrorDetail() ); 
+    
+} 
 string itemType = myItem.getProperty("itemType");
 string itemNum = myItem.getProperty("item_number"); 
 string major_rev = myItem.getProperty("major_rev"); 
-string fileString = itemNum + "-" + major_rev + "." + fileExtension; // e.g. A123456-A.xls
+string fileString = itemNum + "-" + major_rev + "." + fileExtension; // e.g. A123456-A01-3.xls
 
 // Specify the path for storing the file temporary on the server
 string path = @"C:\\temp\\SSRS\\" + fileString;
 
-targetURL = targetURL + "?%2f" + subfolder + 
-                        "%2f" + report + 
-                        "&rs:Command=" + command + 
-                        "&id=" + itemID + 
-                        "&rs:Format=" + format  ;
+targetURL = reportServerURL +   
+            "?%2f" + subfolder + 
+            "%2f" + report + 
+            "&rs:Command=" + command + 
+            "&id=" + itemID + 
+            "&rs:Format=" + format  ;
   
-System.Net.HttpWebRequest req = (HttpWebRequest)System.Net.WebRequest.Create( targetURL );
-
-req.Credentials = new System.Net.NetworkCredential(
-    reportUser,
-    reportUserPW,
-    reportUserDomain );
-req.Method = "GET";
-
-System.Net.WebResponse objResponse = (HttpWebResponse)req.GetResponse();
-System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Create);
-System.IO.Stream stream = objResponse.GetResponseStream();
-          
-byte[] buffer = new byte[1024];
-int length = stream.Read(buffer, 0, 1024);
-
-while (length > 0)
+try 
 {
-    fs.Write(buffer, 0, length);
-    length = stream.Read(buffer, 0, 1024);
+    System.Net.HttpWebRequest request = (HttpWebRequest)System.Net.WebRequest.Create( targetURL );
+    request.Credentials = new System.Net.NetworkCredential(
+        reportUser,
+        securedPW,
+        reportUserDomain );
+    request.Method = "GET";
+    
+    System.Net.HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+    if (response.StatusCode == HttpStatusCode.OK)
+    {
+        System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Create);
+        System.IO.Stream stream = response.GetResponseStream();
+              
+        byte[] buffer = new byte[1024];
+        int length = stream.Read(buffer, 0, 1024);
+    
+        while (length > 0)
+        {
+            fs.Write(buffer, 0, length);
+            length = stream.Read(buffer, 0, 1024);
+        }
+        stream.Close();
+        fs.Close();
+    }
+
+} 
+catch (WebException e)
+{
+    using (Stream data = e.Response.GetResponseStream())
+    {
+        string text = new StreamReader(data).ReadToEnd();
+        return inn.newError(e.Status + "\n" + e.Message + "\n" + text);
+    }
+} 
+catch (Exception e)
+{
+    return inn.newError(e.Message);
 }
-stream.Close();
-fs.Close();
 
 // Check-in File to Aras Vault
 Item fileItem = inn.newItem("File", "add");
-
+fileItem.setAttribute("doGetItem", "0");
 fileItem.setProperty("filename", fileString);
 fileItem.attachPhysicalFile(path);
 Item checkin_result = fileItem.apply();
-
-if (checkin_result.isError()){
-    inn.newError("Fehler beim File Check-In. Bitte Admin melden.");
+if (checkin_result.isError())
+{
+    inn.newError("Fehler beim File Check-In. Bitte Admin melden." + checkin_result.getErrorDetail());
 }
+
 // Delete temporary File from Server
-FileInfo fileInfo = new FileInfo(path);
-        fileInfo.Delete();
+try 
+{
+    FileInfo fileInfo = new FileInfo(path);
+    fileInfo.Delete();
+} 
+catch (Exception e) 
+{
+    return inn.newError(e.ToString());
+}
 
 // Create new Document 
 var docItem = inn.newItem("Document","add");
-docItem.setProperty("item_number", "Report-12345"); // todo: Change to use sequence instead of fixed value
+docItem.setAttribute("doGetItem", "0");
+string sequenceVal = inn.getNextSequence("Default Document"); 
+docItem.setAttribute("useInputProperties", "1"); 
+docItem.setProperty("item_number", sequenceVal);
 docItem.setProperty("name", "SSRS Report " + report + " " + fileString );
 docItem.setProperty("description", "This document was automatically generated.");
 
 // Create new Relationship and attach File
 var relItem = inn.newItem("Document File","add");
+relItem.setAttribute("doGetItem", "0");
 relItem.setProperty("related_id",fileItem.getID());
 docItem.addRelationship(relItem) ;
 var resultItem = docItem.apply();
+if (docItem.isError())
+{
+    return inn.newError("An error occured: " + docItem.getErrorDetail() ); 
+} 
+
 var docId = resultItem.getID();
 
 return inn.newResult(docId); // id of new Document
